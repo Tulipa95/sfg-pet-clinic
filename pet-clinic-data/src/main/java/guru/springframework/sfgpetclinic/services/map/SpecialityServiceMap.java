fbe4f4ed06b2e84d777e17a0bdc8829b26d8df81
Laura Pop <marialaura.pop95@gmail.com>
2021-02-21 15:51:20 +0200

    Create Specialty Map Service Impl. Closes #34

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialitesService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Specialty, Long> implements SpecialitesService {
+	@Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
